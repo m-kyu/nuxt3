484933301c8e755af49e1cc3cd61700b58c72d16
chase <53500545+m-kyu@users.noreply.github.com>
2023-04-19 11:16:00 +0900

    asdf

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        <h2>Index</h2>
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+    h2{background: red;}
+</style>
